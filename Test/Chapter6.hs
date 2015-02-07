a6a6f45b05b84bbc0891fa5f00886aec41528b16
kokuyouwind <kokuyouwind@gmail.com>
2015-02-07 17:21:56 +0900

    第6章のテストで、リストの順序が問題ない場合にはテストが通るよう修正

--- Test/Chapter6.hs
+++ Test/Chapter6.hs
@@ -3,15 +3,19 @@
 import Test.Util
 import Test.QuickCheck
 import Test.QuickCheck.Modifiers
+import Data.Function
+import Data.List(sort)
+import qualified Data.Map as Map
 import qualified Src.Chapter6 as Src
 import qualified Answer.Chapter6 as Ans
 
 prop_sortByFrequency (SafeString xs) =  Src.sortByFrequency xs == Ans.sortByFrequency xs
 test_sortByFrequency = quickCheck prop_sortByFrequency
 
-prop_initialMap xss =  Src.initialMap xss' == Ans.initialMap xss'
+prop_initialMap xss =  Src.initialMap xss' `mapEq` Ans.initialMap xss'
     where types = xss::([SafeString])
           xss' = map unwrapSafeString xss
+          mapEq = (==) `on` Map.map sort
 test_initialMap = quickCheck prop_initialMap
 
 prop_infixPalindromicNumber (NonNegative n) =  Src.infixPalindromicNumber n == Ans.infixPalindromicNumber n
